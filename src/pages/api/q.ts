fd9552593a84fc2abca6b9b1aeea45e8c010a472
--global <zapolnoch@gmail.com>
2023-01-24 17:33:38 +0300

    feat(api): search method

---
+++ src/pages/api/q.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { SearchParams, SearchResponse } from "typesense/lib/Typesense/Documents"
+import { z, ZodIssue } from "zod"
+import { collection, Schema } from "@/shared/typesense"
+
+type Keys = keyof Schema
+type Parameters = SearchParams & {
+  q: string
+  query_by:
+    | `${Keys}`
+    | `${Keys}, ${Keys}`
+    | `${Keys}, ${Keys}, ${Keys}`
+    | `${Keys}, ${Keys}, ${Keys}, ${Keys}`
+  exclude_fields: Keys
+  highlight_fields: Keys
+  highlight_affix_num_tokens: number
+}
+
+const querySchema = z.object({
+  q: z
+    .string({ required_error: "Query is required" })
+    .min(2, { message: "Must be 2 or more characters long" }),
+})
+
+type Res = { success: false; error: ZodIssue } | { success: true; result: SearchResponse<Schema> }
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Res>) {
+  const query = querySchema.safeParse(req.query)
+  if (!query.success) {
+    return res.status(200).json({ success: false, error: query.error.issues[0] })
+  }
+
+  const parameters: Parameters = {
+    q: query.data.q,
+    query_by: "title, desc, text",
+    exclude_fields: "text",
+    highlight_fields: "text",
+    highlight_affix_num_tokens: 10,
+  }
+
+  const result = await collection.documents().search(parameters)
+
+  res.status(200).json({ success: true, result })
+}
+
+export default handler
